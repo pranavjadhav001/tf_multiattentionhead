76cd9875eaf4e58505725668657f2ea62829df87
pranav jadhav <pranav@switchon.io>
2021-05-29 20:37:41 +0530

    changed to einstein notations,added unittest
    Signed-off-by: pranav jadhav <pranav@switchon.io>

--- multi_attention_head.py
+++ multi_attention_head.py
@@ -1,29 +1,36 @@
-from keras import backend as K
-from keras.layers import Layer
-import numpy as np
+from tensorflow.python.ops import special_math_ops
+import math
 import tensorflow as tf
-from attention_head import AttentionHead
+from keras.layers import einsum_dense
+#einsumdense use Lambda layer which are non trainable so using base_layer instead of keras.layers.Layer 
+from tensorflow.python.keras.engine.base_layer import Layer
 
 class multiAttentionHead(Layer):
-    def __init__(self,num_heads=10,k_dim=64,**kwargs):
-        self.k_dim = k_dim
+    def __init__(self, num_heads=10, k_dim=64, **kwargs):
+        self.k_dim = self.q_dim = self.v_dim = k_dim
         self.num_heads = num_heads
         super(multiAttentionHead,self).__init__(**kwargs)
-        
-    def build(self,input_shape):
-        input_dim = input_shape[-1]
-        for i in range(self.num_heads):
-            setattr(self,'head_'+str(i),AttentionHead(k_dim=self.k_dim))
-            getattr(self,'head_'+str(i),None).build(input_shape)
-            
-        self.w_o = self.add_weight(name='weight_o',shape=(self.k_dim*self.num_heads,input_dim),\
-                                           initializer = 'normal', trainable = True)
+    
+    def build(self, input_shape):
+        self.f_dim = input_shape[-1]
+        #[B,token,feature_dim]*[feature_dim,num_heads,v_dim/q_dim/k_dim]->[B,token,num_heads,q_dim/k_dim/v_dim]
+        self.query_dense = einsum_dense.EinsumDense('abc,cde->abde', output_shape=[None, self.num_heads, self.q_dim], bias_axes=None)
+        self.key_dense = einsum_dense.EinsumDense('abc,cde->abde', output_shape=[None, self.num_heads, self.k_dim], bias_axes=None)
+        self.value_dense = einsum_dense.EinsumDense('abc,cde->abde', output_shape=[None, self.num_heads, self.v_dim], bias_axes=None)
+        #[B,token,num_heads,v_dim]*[num_heads,v_dim,feature_dim]->[B,token,feature_dim]
+        self.Wo = einsum_dense.EinsumDense('abcd,cde->abe', output_shape=[None, self.f_dim], bias_axes=None)
         super(multiAttentionHead, self).build(input_shape)
     
-    def call(self,input_vec):
-        outputs = []
-        for i in range(self.num_heads):
-            outputs.append(getattr(self,'head_'+str(i),None).call(input_vec))
-        final_z = tf.keras.layers.Concatenate(axis=-1)(outputs)
-        final_z = K.dot(final_z,self.w_o)
-        return final_z      
+    def call(self, input_vec):
+        query = self.query_dense(input_vec)#[B,token,num_heads,q_dim]
+        key = self.key_dense(input_vec)#[B,token,num_heads,k_dim]
+        value = self.value_dense(input_vec)#[B,token,num_heads,v_dim]
+        #[B,token,num_heads,q_dim]*[B,token,num_heads,k_dim]->[B,num_heads,token,token]
+        scaleddotproduct =  special_math_ops.einsum('abcd,aecd->acbe', query, key)
+        scaleddotproduct = tf.math.divide(scaleddotproduct, float(math.sqrt(self.k_dim)))
+        softmax = tf.nn.softmax(scaleddotproduct, axis=-1)
+        #[B,num_heads,token,token]*[B,token,num_heads,v_dim]->[B,token,num_heads,v_dim]
+        softmax_value = special_math_ops.einsum('acbe,aecd->abcd', softmax, value)
+        #[B,token,num_heads,v_dim]*[num_heads,v_dim,feature_dim]->[B,token,feature_dim]
+        final = self.Wo(softmax_value)
+        return final
--- test_multi_attention_head.py
+++ test_multi_attention_head.py
@@ -1,53 +1,29 @@
 import numpy as np
 import unittest
 import keras.backend as K
+from tensorflow.keras.layers import MultiHeadAttention
 import tensorflow as tf
-import scipy
 from multi_attention_head import multiAttentionHead
 
 class TestmultiAttentionHead(unittest.TestCase):
 	def test_output(self):
+		w_q = np.random.rand(5,3,5).astype(np.float32)
+		w_k = np.random.rand(5,3,5).astype(np.float32)
+		w_v = np.random.rand(5,3,5).astype(np.float32)
+		w_o = np.random.rand(3,5,5).astype(np.float32)
 		input_vec = np.array([[[0.9,0.2,-0.7,1.6,2.2],\
 			[3.2,0.1,-0.5,-1.2,4.0]]]).astype(np.float64)
-		w_q = np.array([[0.6,0.4],\
-		                [-0.4,3.2],\
-		                [1.2,3.1],\
-		                [2.4,1.8],\
-		                [-2.2,0.5]]).astype(np.float64)
-		w_k = np.array([[1.8,-0.6],\
-		                [0.8,0.8],\
-		                [3.3,-0.1],\
-		                [-2.4,0.6],\
-		                [2.6,0.9]]).astype(np.float64)
-		w_v = np.array([[0.6,0.4],\
-		                [-0.4,3.2],\
-		                [1.2,3.1],\
-		                [2.4,1.8],\
-		                [-2.2,0.5]]).astype(np.float64)
-		w_o = np.array([[0.5,-1.2,0.4,0.6,3.2],
-		               [2.3,0.2,0.8,0.6,2],
-		               [0.5,-1.2,0.4,-1.6,3.2],
-		               [0.5,5.2,0.4,4.6,0.2],
-		               [0.5,6.2,0.4,0.6,0.5],
-		               [0.5,8.2,-0.4,3.0,-0.2]])
-		q = np.dot(input_vec,w_q)
-		k = np.dot(input_vec,w_k)
-		v = np.dot(input_vec,w_v)
-		score = K.batch_dot(tf.convert_to_tensor(q),tf.convert_to_tensor(k),axes=2)
-		score = K.get_value(score)/np.sqrt(2)
-		score_soft = scipy.special.softmax(score,axis=2)
-		tf_score = K.batch_dot(tf.convert_to_tensor(score_soft),tf.convert_to_tensor(v))
-		numpy_score = K.get_value(tf_score)
-		all_numpy_score = np.tile(numpy_score,[1,1,3])
-		final_z = np.dot(all_numpy_score,w_o)
-		dummy = multiAttentionHead(num_heads=3,k_dim=2)
-		dummy.build(input_shape=(1,2,5))
-		dummy.set_weights([w_o,w_q,w_k,w_v,\
-                 		 w_q,w_k,w_v,\
-                  		w_q,w_k,w_v])
-		keras_score = dummy.call(tf.convert_to_tensor(input_vec,dtype=tf.float32))
-		keras_score = K.get_value(keras_score)
-		self.assertEqual(np.allclose(final_z,keras_score,0.001,0.001),True)
+		input_tensor = tf.keras.Input(shape=[2, 5])
+		keras_attention = MultiHeadAttention(num_heads=3,key_dim=5,use_bias=False)
+		output_tensor = keras_attention(input_tensor,input_tensor)
+		keras_attention.set_weights([w_q,w_k,w_v,w_o])
+		keras_attention_output = keras_attention(input_vec,input_vec)
+
+		myAttention = multiAttentionHead(num_heads=3,k_dim=5)
+		output_tensor = myAttention(input_tensor)
+		myAttention.set_weights([w_q,w_k,w_v,w_o])
+		myAttention_output = myAttention(input_vec)
+		self.assertEqual(np.allclose(myAttention_output.numpy(),keras_attention_output.numpy(),0.001,0.001),True)
 
 if __name__ == '__main__':
 	unittest.main()
