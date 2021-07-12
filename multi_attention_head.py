619a434f9bcf0462f91ab0689121024b899ecb98
pranav jadhav <pranav@switchon.io>
2021-07-12 11:15:40 +0530

    added bias and attention mask ,testcases for each

--- multi_attention_head.py
+++ multi_attention_head.py
@@ -1,33 +1,44 @@
 from tensorflow.python.ops import special_math_ops
 import math
+import numpy as np
 import tensorflow as tf
-from keras.layers import einsum_dense
+from tensorflow.python.keras.layers import einsum_dense
 #einsumdense use Lambda layer which are non trainable so using base_layer instead of keras.layers.Layer 
 from tensorflow.python.keras.engine.base_layer import Layer
 
 class multiAttentionHead(Layer):
-    def __init__(self, num_heads=10, k_dim=64, **kwargs):
+    def __init__(self, num_heads=10, k_dim=64, use_bias=False, **kwargs):
         self.k_dim = self.q_dim = self.v_dim = k_dim
         self.num_heads = num_heads
+        self.use_bias = use_bias
         super(multiAttentionHead,self).__init__(**kwargs)
     
     def build(self, input_shape):
         self.f_dim = input_shape[-1]
         #[B,token,feature_dim]*[feature_dim,num_heads,v_dim/q_dim/k_dim]->[B,token,num_heads,q_dim/k_dim/v_dim]
-        self.query_dense = einsum_dense.EinsumDense('abc,cde->abde', output_shape=[None, self.num_heads, self.q_dim], bias_axes=None)
-        self.key_dense = einsum_dense.EinsumDense('abc,cde->abde', output_shape=[None, self.num_heads, self.k_dim], bias_axes=None)
-        self.value_dense = einsum_dense.EinsumDense('abc,cde->abde', output_shape=[None, self.num_heads, self.v_dim], bias_axes=None)
-        #[B,token,num_heads,v_dim]*[num_heads,v_dim,feature_dim]->[B,token,feature_dim]
-        self.Wo = einsum_dense.EinsumDense('abcd,cde->abe', output_shape=[None, self.f_dim], bias_axes=None)
+        if self.use_bias:
+            self.query_dense = einsum_dense.EinsumDense('abc,cde->abde', output_shape=[None, self.num_heads, self.q_dim], bias_axes='de')
+            self.key_dense = einsum_dense.EinsumDense('abc,cde->abde', output_shape=[None, self.num_heads, self.k_dim], bias_axes='de')
+            self.value_dense = einsum_dense.EinsumDense('abc,cde->abde', output_shape=[None, self.num_heads, self.v_dim], bias_axes='de')
+            #[B,token,num_heads,v_dim]*[num_heads,v_dim,feature_dim]->[B,token,feature_dim]
+            self.Wo = einsum_dense.EinsumDense('abcd,cde->abe', output_shape=[None, self.f_dim], bias_axes='e')
+        else:
+            self.query_dense = einsum_dense.EinsumDense('abc,cde->abde', output_shape=[None, self.num_heads, self.q_dim])
+            self.key_dense = einsum_dense.EinsumDense('abc,cde->abde', output_shape=[None, self.num_heads, self.k_dim])
+            self.value_dense = einsum_dense.EinsumDense('abc,cde->abde', output_shape=[None, self.num_heads, self.v_dim])
+            #[B,token,num_heads,v_dim]*[num_heads,v_dim,feature_dim]->[B,token,feature_dim]
+            self.Wo = einsum_dense.EinsumDense('abcd,cde->abe', output_shape=[None, self.f_dim])
         super(multiAttentionHead, self).build(input_shape)
     
-    def call(self, input_vec):
+    def call(self, input_vec, attention_mask=None):
         query = self.query_dense(input_vec)#[B,token,num_heads,q_dim]
         key = self.key_dense(input_vec)#[B,token,num_heads,k_dim]
         value = self.value_dense(input_vec)#[B,token,num_heads,v_dim]
         #[B,token,num_heads,q_dim]*[B,token,num_heads,k_dim]->[B,num_heads,token,token]
         scaleddotproduct =  special_math_ops.einsum('abcd,aecd->acbe', query, key)
         scaleddotproduct = tf.math.divide(scaleddotproduct, float(math.sqrt(self.k_dim)))
+        if attention_mask:
+            scaleddotproduct = tf.where(attention_mask, scaleddotproduct, -1e9)
         softmax = tf.nn.softmax(scaleddotproduct, axis=-1)
         #[B,num_heads,token,token]*[B,token,num_heads,v_dim]->[B,token,num_heads,v_dim]
         softmax_value = special_math_ops.einsum('acbe,aecd->abcd', softmax, value)
--- test_multi_attention_head.py
+++ test_multi_attention_head.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 from multi_attention_head import multiAttentionHead
 
 class TestmultiAttentionHead(unittest.TestCase):
-	def test_output(self):
+	def test_output1(self):
 		w_q = np.random.rand(5,3,5).astype(np.float32)
 		w_k = np.random.rand(5,3,5).astype(np.float32)
 		w_v = np.random.rand(5,3,5).astype(np.float32)
@@ -19,11 +19,59 @@ class TestmultiAttentionHead(unittest.TestCase):
 		keras_attention.set_weights([w_q,w_k,w_v,w_o])
 		keras_attention_output = keras_attention(input_vec,input_vec)
 
-		myAttention = multiAttentionHead(num_heads=3,k_dim=5)
+		myAttention = multiAttentionHead(num_heads=3,k_dim=5,use_bias=False)
 		output_tensor = myAttention(input_tensor)
 		myAttention.set_weights([w_q,w_k,w_v,w_o])
 		myAttention_output = myAttention(input_vec)
 		self.assertEqual(np.allclose(myAttention_output.numpy(),keras_attention_output.numpy(),0.001,0.001),True)
+
+	def test_output2(self):
+		w_q = np.random.rand(5,3,5).astype(np.float32)
+		w_k = np.random.rand(5,3,5).astype(np.float32)
+		w_v = np.random.rand(5,3,5).astype(np.float32)
+		w_o = np.random.rand(3,5,5).astype(np.float32)
+		b_q = np.random.rand(3,5).astype(np.float32)
+		b_k = np.random.rand(3,5).astype(np.float32)
+		b_v = np.random.rand(3,5).astype(np.float32)
+		b_o = np.random.rand(5).astype(np.float32)
+		input_vec = np.array([[[0.9,0.2,-0.7,1.6,2.2],\
+			[3.2,0.1,-0.5,-1.2,4.0]]]).astype(np.float64)
+		input_tensor = tf.keras.Input(shape=[2, 5])
+		keras_attention = MultiHeadAttention(num_heads=3,key_dim=5,use_bias=True)
+		output_tensor = keras_attention(input_tensor,input_tensor)
+		keras_attention.set_weights([w_q,b_q,w_k,b_k,w_v,b_v,w_o,b_o])
+		keras_attention_output = keras_attention(input_vec,input_vec)
+
+		myAttention = multiAttentionHead(num_heads=3,k_dim=5,use_bias=True)
+		output_tensor = myAttention(input_tensor)
+		myAttention.set_weights([w_q,b_q,w_k,b_k,w_v,b_v,w_o,b_o])
+		myAttention_output = myAttention(input_vec)
+		self.assertEqual(np.allclose(myAttention_output.numpy(),keras_attention_output.numpy(),0.001,0.001),True)
+
+	def test_output3(self):
+		w_q = np.random.rand(5,3,5).astype(np.float32)
+		w_k = np.random.rand(5,3,5).astype(np.float32)
+		w_v = np.random.rand(5,3,5).astype(np.float32)
+		w_o = np.random.rand(3,5,5).astype(np.float32)
+		b_q = np.random.rand(3,5).astype(np.float32)
+		b_k = np.random.rand(3,5).astype(np.float32)
+		b_v = np.random.rand(3,5).astype(np.float32)
+		b_o = np.random.rand(5).astype(np.float32)
+		attention_mask = [[[True,False],[True,False]]]
+		input_vec = np.array([[[0.9,0.2,-0.7,1.6,2.2],\
+			[3.2,0.1,-0.5,-1.2,4.0]]]).astype(np.float64)
+		input_tensor = tf.keras.Input(shape=[2, 5])
+		keras_attention = MultiHeadAttention(num_heads=3,key_dim=5,use_bias=True)
+		output_tensor = keras_attention(input_tensor,input_tensor,)
+		keras_attention.set_weights([w_q,b_q,w_k,b_k,w_v,b_v,w_o,b_o])
+		keras_attention_output = keras_attention(input_vec,input_vec,attention_mask=tf.cast((attention_mask),tf.int32))
+
+		myAttention = multiAttentionHead(num_heads=3,k_dim=5,use_bias=True)
+		output_tensor = myAttention(input_tensor)
+		myAttention.set_weights([w_q,b_q,w_k,b_k,w_v,b_v,w_o,b_o])
+		myAttention_output = myAttention(input_vec,attention_mask=attention_mask)
+		self.assertEqual(np.allclose(myAttention_output.numpy(),keras_attention_output.numpy(),0.001,0.001),True)
+		tf.keras.backend.clear_session()
 
 if __name__ == '__main__':
 	unittest.main()
